b074332aab50c077a83e2f860fda0048acb3709e
Tinatojo Dino <tinatojo2@gmail.com>
2024-06-25 21:51:51 +0300

    add env variable

--- spotpy.py
+++ spotpy.py
@@ -11,10 +11,19 @@
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="204bcfe708d64cc7b61b1cdd929e19ac",
-                                               client_secret="26b7e34d3dfe43bba3ea21192a5aa5b3",
-                                               redirect_uri="http://localhost:8888/callback",
+load_dotenv()
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_secret = os.getenv("SPOTIFY_SECRET")
+spotify_redirect_url = os.getenv("SPOTIFY_REDIRECT_URL")
+
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
+                                               client_secret=spotify_secret,
+                                               redirect_uri=spotify_redirect_url,
                                                scope="user-library-read"))
 
 results = sp.current_user_saved_tracks()
